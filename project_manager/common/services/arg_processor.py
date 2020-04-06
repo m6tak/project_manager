b045ccbfef236584b0f6d5d38173a075744967f9
Michał Szóstak <mszostakk01@gmail.com>
2020-04-06 17:27:42 +0200

    add adtitional optional parameters to integrate vscode and file explorer

--- project_manager/common/services/arg_processor.py
+++ project_manager/common/services/arg_processor.py
@@ -5,6 +5,8 @@ parser.add_argument('project_name', help='name of your project')
 parser.add_argument('tech', help='main project technology')
 parser.add_argument('--no_repo', action='store_true', help='switch for git repo initialization')
 parser.add_argument('--repo_name', action='store', help='repository name')
+parser.add_argument('--open_vsc', action='store_true', help='open repository with vs code')
+parser.add_argument('--open_dir', action='store_true', help='open repository in file explorer (windows only)')
 
 
 def parse():
--- project_manager/pm.py
+++ project_manager/pm.py
@@ -13,11 +13,12 @@ def run():
 
     if args.no_repo:
         print("New {tech} project created: {path}".format(tech=args.tech, path=paths['proj']))
-        exit(0)
     else:
         print("New {tech} project created: {path}".format(tech=args.tech, path=paths['proj']))
         init_repo(paths['repo'], args.project_name)
         print("Git repository initialized with readme.md")
+        
+    additional_actions(args, paths['repo'])
         
 
 
@@ -25,11 +26,9 @@ def create_dirs(cfg, args):
     root  = cfg['root']
     proj_schema = cfg['proj_schema']
     repo_schema = cfg['repo_schema']
-    repo_name = args.project_name if not args.repo_name else args.repo_name
+    repo_name = args.project_name if args.repo_name is None else args.repo_name
     proj_path = proj_schema.format(root=root, tech=args.tech, proj_repo=repo_name, proj_root=args.project_name)
-    repo_path = repo_schema.format(root=root, tech=args.tech, proj_repo=args.repo_name)
-    print(proj_path)
-    print(repo_path)
+    repo_path = repo_schema.format(root=root, tech=args.tech, proj_repo=repo_name)
     try:
         os.makedirs(proj_path)
         return {'proj': proj_path, 'repo': repo_path}
@@ -42,6 +41,13 @@ def init_repo(repo_path, project_name):
     with open(repo_path + '\\readme.md', 'w') as readme:
         readme.write('# {project_name}\n'.format(project_name=project_name))
     os.system('cd {path} && git init && git add . && git commit -m \"initial commit\"'.format(path=repo_path))
+
+def additional_actions(args, repo_path):
+    if args.open_vsc:
+        os.system('cd {path} && code .'.format(path=repo_path))
+    
+    if args.open_dir:
+        os.system('start {path}'.format(path=repo_path))
 
 if __name__ == '__main__':
     run()
