0d85ce44012e9a48e0db350c611b24f4f456221b
Michał Szóstak <mszostakk01@gmail.com>
2020-04-09 10:25:17 +0200

    add default config in users home directory

--- project_manager/common/services/config.py
+++ project_manager/common/services/config.py
@@ -1,6 +1,20 @@
 import json
+import os
 
 def load():
-    with open('config.json') as config_file:
+    cfg_path = os.path.join(os.path.expand_user('~'), '.project_manager/config.json')
+    if not os.path.exists(cfg_path):
+        os.makedirs(cfg_path)
+        default_cfg = {
+            'root': os.path.join(os.path.expanduser('~'), "repositories"),
+            'proj_schema': "{root}\\{tech}\\{proj_repo}\\{proj_root}",
+            'repo_schema': "{root}\\{tech}\\{proj_repo}",
+            'custom_schemas': [
+                'default': [],
+                'my_custom_schema': [],
+            ]
+        }
+
+    with open(cfg_path) as config_file:
         return json.load(config_file)
 
